a0dfd6def190dd31b1269ad49d14875ad74016a1
Septem <septem2005@126.com>
2018-08-12 01:11:34 +0800

    add text proceess

--- src/main/java/my/hehe/web/WebApplication.java
+++ src/main/java/my/hehe/web/WebApplication.java
@@ -2,26 +2,26 @@ package my.hehe.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+*/
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-public class WebApplication extends SpringBootServletInitializer {
+public class WebApplication /*extends SpringBootServletInitializer*/ {
     private static ApplicationContext applicationContext;
 
     public static void main(String[] args) {
         applicationContext = SpringApplication.run(WebApplication.class, args);
     }
 
-    @Override
+    /*@Override
     protected SpringApplicationBuilder configure(
             SpringApplicationBuilder builder) {
         // TODO Auto-generated method stub
         return builder.sources(WebApplication.class);
-    }
+    }*/
 
     public static ApplicationContext getApplicationContext() {
         return applicationContext;
--- src/main/java/my/hehe/web/atomServer/msg/service/TextProcceServer.java
+++ src/main/java/my/hehe/web/atomServer/msg/service/TextProcceServer.java
@@ -1,11 +1,13 @@
 package my.hehe.web.atomServer.msg.service;
 
 import org.json.JSONObject;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Service
+@Profile("atomSrv")
 public class TextProcceServer {
     public JSONObject excute(JSONObject data){
         return  null;
